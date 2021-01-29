b3424da4d25dc572c3ef4eea55a2f41c0eab87cf
Collin <collinmcmahon2014@yahoo.com>
2021-01-29 00:13:42 -0500

    added backend functionality

--- server/server.js
+++ server/server.js
@@ -5,7 +5,13 @@ const port = 3000;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve('public')));
+
+app.get('/game_carousel_info', (req, res) => {
+  console.log(req.query.id);
+});
 
 app.listen(port, () => {
   console.log(`server listening at http://localhost:${port}`);
